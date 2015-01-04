569fac7cc027672f8a5ae4d32fb5c112643b49e2
maruloop <marumaru.niconico@gmail.com>
2015-01-04 19:44:48 +0900

    remove println

--- app/domain/count/PlayCounter.scala
+++ app/domain/count/PlayCounter.scala
@@ -8,14 +8,11 @@ class PlayCounter {
   def  incrementPlayCount(name: String, filename: String): Int =
     DB.withTransaction (implicit connection  => {
       if(!isExistVoiceBank(name)){
-        println("not exsist vb")
         insertNewVoiceBank(name)
         insertNewFilename(name, filename)
       }else if(!isExistFilename(name, filename)){
-        println("not exsist filename")
         insertNewFilename(name, filename)
       }else{
-        println("exsist!!!")
         val count: Int = getCount(name, filename)
         updateCount(name, filename, count+1)
         count + 1
