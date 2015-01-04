f4cd5ad2228329b615099a8e318363f962d91e5f
maruloop <marumaru.niconico@gmail.com>
2015-01-04 22:45:25 +0900

    test transaction

--- app/domain/count/PlayCounter.scala
+++ app/domain/count/PlayCounter.scala
@@ -3,18 +3,107 @@ package domain.count
 import domain.Dao._
 import play.api.db._
 import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
+import anorm.Column._
+import java.sql.Connection
+import anorm.NamedParameter.symbol
 
 class PlayCounter {
   def  incrementPlayCount(name: String, filename: String): Int =
     DB.withTransaction (implicit connection  => {
-      if(!isExistVoiceBank(name)){
-        insertNewVoiceBank(name)
-        insertNewFilename(name, filename)
-      }else if(!isExistFilename(name, filename)){
-        insertNewFilename(name, filename)
+      val isExsistVoicebank = SQL(
+        """
+          SELECT EXISTS(
+          SELECT * FROM voicebank WHERE name = {name}
+          )
+        """
+      ).on(
+        'name -> name
+      ).as(scalar[Int].single) != 0
+      
+      val isExistFilename = SQL(
+        """
+          SELECT EXISTS(
+          SELECT * FROM activity WHERE
+          filename = {filename} AND id = ( SELECT id FROM voicebank WHERE name = {name} )
+          )
+        """
+      ).on(
+        'name -> name,
+        'filename -> filename
+      ).as(scalar[Int].single) != 0
+      
+      if(!isExsistVoicebank){
+            SQL(
+        """
+          INSERT INTO voicebank (name) VALUES ({name})
+        """
+      ).on(
+        'name -> name
+      ).executeInsert()
+      val id: Int = SQL(
+        """
+          SELECT id FROM voicebank WHERE
+          name = {name}
+        """
+      ).on(
+        'name -> name
+      ).as(scalar[Int].single)
+            SQL(
+        """
+          INSERT INTO activity (id, filename) VALUES ({id}, {filename})
+        """
+      ).on(
+        'id -> id,
+        'filename -> filename
+      ).executeInsert()
+      1
+      }else if(!isExistFilename){
+            SQL(
+        """
+          INSERT INTO voicebank (name) VALUES ({name})
+        """
+      ).on(
+        'name -> name
+      ).executeInsert()
+      val id: Int = SQL(
+        """
+          SELECT id FROM voicebank WHERE
+          name = {name}
+        """
+      ).on(
+        'name -> name
+      ).as(scalar[Int].single)
+            SQL(
+        """
+          INSERT INTO activity (id, filename) VALUES ({id}, {filename})
+        """
+      ).on(
+        'id -> id,
+        'filename -> filename
+      ).executeInsert()
+      1
       }else{
-        val count: Int = getCount(name, filename)
-        updateCount(name, filename, count+1)
+        val count: Int = SQL(
+        """
+          SELECT count FROM activity WHERE
+          filename = {filename} AND id = ( SELECT id FROM voicebank WHERE name = {name} )
+        """
+      ).on(
+        'name -> name,
+        'filename -> filename
+      ).as(scalar[Int].single)
+            SQL(
+        """
+          UPDATE activity SET count = {count}
+          WHERE filename = {filename} AND id = ( SELECT id FROM voicebank WHERE name = {name} )
+        """
+      ).on(
+        'name -> name,
+        'filename -> filename,
+        'count -> count
+      ).executeUpdate()
         count + 1
       }
     })
