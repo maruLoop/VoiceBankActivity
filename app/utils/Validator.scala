fd386f29220fdc42c110ddd244e921e7cf9f9f78
maruloop <marumaru.niconico@gmail.com>
2015-01-04 20:07:42 +0900

    fix typo

--- app/utils/Validator.scala
+++ app/utils/Validator.scala
@@ -2,5 +2,5 @@ package utils
 
 object Validator {
   // TODO なぜかdomain.DllFunctionType.GET_PCM_DATA.getCodeが出来ない
-  def isValidFunctionGetPcmData(param: String): Boolean = param == "getPcmData"
+  def isValidFunctionGetPcmData(param: String): Boolean = param == "getpcmdata"
 }
