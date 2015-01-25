bd96d600e270d38664ad86faf30d49f9be675b56
maruloop <marumaru.niconico@gmail.com>
2015-01-25 21:25:44 +0900

    fix pushstate

--- public/javascripts/render-voicebanks.js
+++ public/javascripts/render-voicebanks.js
@@ -3,6 +3,13 @@ $(function () {
         'selectedText': 'cat'
     });
     
+    $(window).on('popstate', function(jqevent) {
+      var state = jqevent.originalEvent.state; // stateオブジェクト
+      page = state;
+      showVoicebanksWithoutPushState(state);
+    });
+    
+    history.replaceState(0);
     showVoicebanks(page);
 });
 
@@ -27,7 +34,7 @@ var renderVoicebanks = function(values){
     $('#voicebanks').html(template(values));
 }
 
-var showVoicebanks = function(pageNum){
+var showVoicebanksWithoutPushState = function(pageNum){
 	$.ajax({
 	    url: "/json/getAllVoicebanks",
 	    data: {
@@ -42,7 +49,44 @@ var showVoicebanks = function(pageNum){
 	}).done(function(data){
 		page = data.pageNow; // Global
 		renderVoicebanks(data);
-		history.pushState("","","/voicebanks?page="+data.pageNow);
+//		history.pushState(data.pageNow, null,"/voicebanks?page="+data.pageNow);
+	}).fail(function(e){
+	    console.log('error!!!');
+	    console.log(e);
+	}).always(function(){
+	    $('.page-button').on('click', function(){
+	    	showVoicebanks($(this).data('page'));
+	    });
+	    
+	    $pageSize.on('change', function(){
+	    	showVoicebanks(0);
+	    });
+	    
+	    $sortOrder.on('change', function(){
+	    	showVoicebanks(0);
+	    });
+	});
+	
+}
+
+var showVoicebanks = function(pageNum){
+	$.ajax({
+	    url: "/json/getAllVoicebanks",
+	    data: {
+	    	page: pageNum,
+	    	pageSize: $pageSize.val(),
+	    	sortCode: $sortOrder.find(':selected').data('sort'),
+	    	orderCode: $sortOrder.find(':selected').data('order')
+	    	},
+	    beforeSend: function(){
+			            $('#voicebanks').html($("img").attr("src","../images/ajax-loader.gif"));
+		            }
+	}).done(function(data){
+		renderVoicebanks(data);
+		if(page != data.pageNow){
+			history.pushState(data.pageNow, null,"/voicebanks?page="+data.pageNow);
+		}
+		page = data.pageNow; // Global
 	}).fail(function(e){
 	    console.log('error!!!');
 	    console.log(e);
